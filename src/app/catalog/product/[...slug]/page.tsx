24051c5577ee8f95ff8cc199eee03a11fd8bf28b
Victor Carli <victorcarli@FB1311>
2024-02-14 00:04:56 -0300

    feat: add loading

--- src/app/catalog/product/[...slug]/page.tsx
+++ src/app/catalog/product/[...slug]/page.tsx
@@ -4,7 +4,15 @@ interface ProductProps {
   };
 }
 
-export default function Product({ params }: ProductProps) {
+// Server Components => A gente não usa JS no lado do client
+// Client Components => O JS é enviado ao navegador (client)
+
+// Streaming SSR => Ler/escrever dados de forma parcial + SSR
+// Renderizar um componente pelo lado do servidor de forma PARCIAL
+
+export default async function Product({ params }: ProductProps) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
   const [productId, size, color] = params.slug;
 
   return (
